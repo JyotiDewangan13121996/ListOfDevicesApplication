512f962e919dac17634b56be44354f815f2dd1bd
JyotiDewangan13121996 <58210365+JyotiDewangan13121996@users.noreply.github.com>
2020-04-02 17:27:22 +0530

    Update ManufacturerServiceTester.java

--- JUnitFramework/com.app.serviceTester/ManufacturerServiceTester.java
+++ JUnitFramework/com.app.serviceTester/ManufacturerServiceTester.java
@@ -1,55 +1,76 @@
 package com.app.serviceTester;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.app.dao.ManufacturerDaoImplementation;
 import com.app.pojos.Manufacturer;
 import com.app.service.IManufacturerService;
+import com.app.service.ManufacturerServiceImplementation;
 
+
+@RunWith(SpringRunner.class)
 public class ManufacturerServiceTester 
 {
-  
-	@Autowired
-	private IManufacturerService manufacturerServiceTester;
+ 
+	@InjectMocks
+	private ManufacturerServiceImplementation manufacturerServiceTester ;
 
+	@Mock
+	private ManufacturerDaoImplementation manufacturerdao ;
 	
-	private Integer manufacturerIdToBeTested ;
+	private Integer manufacturerIdToBeTested =1 ;
 	
-	
-	@Before
-	public void initialize()
-	{
-		
-		Scanner sc = new Scanner(System.in) ;
-
-		System.out.println("Enter the manufacturer id which is to be tested :");
-		this.manufacturerIdToBeTested = sc.nextInt() ;
-		
-	}
 	
 	
 	// To test the getManufacturerDetails() method by manufacturer service layer . 
-	
-	public void testGetManufacturerDetails(int manufacturerId)
+	@Test
+	public void testGetManufacturerDetails()
 	{
-		manufacturerId =this. manufacturerIdToBeTested ;
 		
-		assertNotNull(manufacturerServiceTester.getManufacturerDetails(manufacturerId));
+		Manufacturer manufacturer= new Manufacturer();
+		manufacturer.setLocationOfManufacturing("Pune");
+		manufacturer.setManufactureId(1);
+		manufacturer.setManufacturerName("Samsung");
+		
+		Mockito.when(manufacturerdao.getManufacturerDetails(1) ).thenReturn(manufacturer);
+		
+		assertThat(manufacturerServiceTester.getManufacturerDetails(1)).isEqualTo(manufacturer);
+		
+		//assertNotNull(manufacturerServiceTester.getManufacturerDetails(this.manufacturerIdToBeTested));
 		
 	}
 	
 	
 	// To test the updateManufacturerDetails() method by manufacturer service layer . 
-	
-	public void testUpdateManufacturerDetails(Manufacturer updatedManufacturer)
+	@Test
+	public void testUpdateManufacturerDetails()
 	{
-		updatedManufacturer = manufacturerServiceTester.getManufacturerDetails(this.manufacturerIdToBeTested);
+		Manufacturer manufacturer= new Manufacturer();
+		manufacturer.setLocationOfManufacturing("Pune");
+		manufacturer.setManufactureId(1);
+		manufacturer.setManufacturerName("Samsung");
 		
-		assertNotNull(manufacturerServiceTester.updateManufacturerDetails(updatedManufacturer));
+		Mockito.when(manufacturerdao.updateManufacturerDetails(manufacturer)).thenReturn(manufacturer);
+		
+		assertThat(manufacturerServiceTester.updateManufacturerDetails(manufacturer)).isEqualTo(manufacturer);
+		
+		// Manufacturer updatedManufacturer = manufacturerServiceTester.getManufacturerDetails(this.manufacturerIdToBeTested);
+		
+		//assertNotNull(manufacturerServiceTester.updateManufacturerDetails(updatedManufacturer));
 	}
 	
 	
