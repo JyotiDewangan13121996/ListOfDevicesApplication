fa8b6abda937b18d42ff563eb06703b6607fed43
JyotiDewangan13121996 <58210365+JyotiDewangan13121996@users.noreply.github.com>
2020-04-02 11:56:01 +0530

    Update ManufacturerServiceTester.java

--- JUnitFramework/com.app.serviceTester/ManufacturerServiceTester.java
+++ JUnitFramework/com.app.serviceTester/ManufacturerServiceTester.java
@@ -2,6 +2,9 @@ package com.app.serviceTester;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Scanner;
+
+import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.app.pojos.Manufacturer;
@@ -11,13 +14,30 @@ public class ManufacturerServiceTester
 {
   
 	@Autowired
-	private IManufacturerService manufacturerServiceTester ;
+	private IManufacturerService manufacturerServiceTester;
+
+	
+	private Integer manufacturerIdToBeTested ;
+	
+	
+	@Before
+	public void initialize()
+	{
+		
+		Scanner sc = new Scanner(System.in) ;
+
+		System.out.println("Enter the manufacturer id which is to be tested :");
+		this.manufacturerIdToBeTested = sc.nextInt() ;
+		
+	}
 	
 	
 	// To test the getManufacturerDetails() method by manufacturer service layer . 
 	
 	public void testGetManufacturerDetails(int manufacturerId)
 	{
+		manufacturerId =this. manufacturerIdToBeTested ;
+		
 		assertNotNull(manufacturerServiceTester.getManufacturerDetails(manufacturerId));
 		
 	}
@@ -27,6 +47,8 @@ public class ManufacturerServiceTester
 	
 	public void testUpdateManufacturerDetails(Manufacturer updatedManufacturer)
 	{
+		updatedManufacturer = manufacturerServiceTester.getManufacturerDetails(this.manufacturerIdToBeTested);
+		
 		assertNotNull(manufacturerServiceTester.updateManufacturerDetails(updatedManufacturer));
 	}
 	
