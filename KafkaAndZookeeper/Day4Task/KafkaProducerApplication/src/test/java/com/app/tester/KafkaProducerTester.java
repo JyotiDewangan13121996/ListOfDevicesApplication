63c1722c9e33d4405c9e0aeddef2492c32b3e85d
JyotiDewangan13121996 <58210365+JyotiDewangan13121996@users.noreply.github.com>
2020-04-16 21:15:42 +0530

    Add files via upload

---
+++ KafkaAndZookeeper/Day4Task/KafkaProducerApplication/src/test/java/com/app/tester/KafkaProducerTester.java
@@ -0,0 +1,43 @@
+package com.app.tester;
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.app.controller.KafkaController;
+import com.app.service.KafkaProducer;
+
+@RunWith(SpringRunner.class)
+public class KafkaProducerTester 
+{
+  
+   @InjectMocks
+   private KafkaController kafkaController ;
+   
+   
+   @Mock
+   private KafkaProducer producer;
+   
+   
+   @Test
+   public void testSendMessageToKafkaTopic()
+   {
+	   String message = "Some Record" ;
+	   
+	   Mockito.when(this.producer.sendMessage(message)).thenReturn("Published Successfully!");
+	   
+	   assertThat(kafkaController.sendMessageToKafkaTopic(message)).isEqualTo("Published Successfully!");
+	  
+   }
+   
+   
+   
+}
